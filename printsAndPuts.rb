4c70f6e61ddba49f193aebb67b839baeccdabace
isvaldo-santos <isvaldosantos@outlook.com>
2022-05-11 09:31:25 -0300

    Inicio

---
+++ printsAndPuts.rb
@@ -0,0 +1,16 @@
+
+# Puts e Prints 
+
+puts ""
+
+print "1.Encontre o valor de X no triangulo retângulo:"
+
+puts ""
+puts ""
+
+puts "    /|"
+puts "   / |"
+puts "30/  |X"
+puts " /   |"
+puts "/____|"
+puts "  20 "
---
+++ variables.rb
@@ -0,0 +1,10 @@
+
+# Variables
+
+character_name = "Isvaldo"
+character_age = "21"
+
+puts ("My name is " + character_name)
+puts ("I Am " + character_age + " years old.")
+puts ("I Really like being " + character_age)
+puts ("but I don't like being named " + character_name)
