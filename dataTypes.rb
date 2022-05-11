9e891ac4a472c05c6a8be1b80b932f87e98df2e2
isvaldo-santos <isvaldosantos@outlook.com>
2022-05-11 09:35:23 -0300

    Data Types

--- dataTypes.rb
+++ dataTypes.rb
@@ -1,2 +1,10 @@
 #DataTypes
 
+name = "Isvaldo"    #String
+occupation = "English Professor"    #String
+age = 21    #Intenger
+is_male = true    #Boolean    
+is_female = false    #Bolean
+height = 1.72    #float
+marital_status = nil    #Means that is doesn't have a value, like null in javascript.
+
